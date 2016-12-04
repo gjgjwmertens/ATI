850730d2a5d1242cee8430afe40a99c03771cd4c
gjgjwmertens <G.J.G.J.W.Mertens@uva.nl>
2016-12-04 14:57:03 +0100

    added KY028 temp sensor

--- public/js/uno.js
+++ public/js/uno.js
@@ -111,62 +111,22 @@ function updateStatus(data) {
       currentValues[data.item].avg.shift();
       currentValues[data.item].avgValue = currentValues[data.item].avg.average();
 
-      $('#' + data.item + '_current_value_field_id').text('(' + temp(data.data) + ')');
+      $('#' + data.item + '_current_value_field_id').text('(' + temp2(data.data) + ')');
    }
 }
 
-function temp(raw) {
-   var r, t;
+function temp2(raw) {
+   var rSense = 63800;
+   var rRef = 75000; // gues ;)
+   var r = (raw / ((1023 - raw) / rSense)) - 150;
+   var A1 = 3.354016E-03;
+   var B1 = 2.460382E-04;
+   var C1 = 3.405377E-06;
+   var D1 = 1.034240E-07;
+   var lr = Math.log(r/rRef);
 
-   r = Math.log(10000 * (1024 / (1024 - (raw - 400))));
-   t = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * r * r)) * r);
-   return Math.round10(t - 273.15, -1);
+   var t = 1 / (A1 + B1 * lr + C1 * lr * lr + D1 * lr * lr *lr);
+   var t = t - 273.15;
+
+   return t.toFixed(2);
 }
-// Closure
-(function() {
-   /**
-    * Decimal adjustment of a number.
-    *
-    * @param {String}  type  The type of adjustment.
-    * @param {Number}  value The number.
-    * @param {Integer} exp   The exponent (the 10 logarithm of the adjustment base).
-    * @returns {Number} The adjusted value.
-    */
-   function decimalAdjust(type, value, exp) {
-      // If the exp is undefined or zero...
-      if (typeof exp === 'undefined' || +exp === 0) {
-         return Math[type](value);
-      }
-      value = +value;
-      exp = +exp;
-      // If the value is not a number or the exp is not an integer...
-      if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
-         return NaN;
-      }
-      // Shift
-      value = value.toString().split('e');
-      value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
-      // Shift back
-      value = value.toString().split('e');
-      return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
-   }
-
-   // Decimal round
-   if (!Math.round10) {
-      Math.round10 = function(value, exp) {
-         return decimalAdjust('round', value, exp);
-      };
-   }
-   // Decimal floor
-   if (!Math.floor10) {
-      Math.floor10 = function(value, exp) {
-         return decimalAdjust('floor', value, exp);
-      };
-   }
-   // Decimal ceil
-   if (!Math.ceil10) {
-      Math.ceil10 = function(value, exp) {
-         return decimalAdjust('ceil', value, exp);
-      };
-   }
-})();
